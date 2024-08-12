62f9f45978a4785c78e58ad9d4c7545e41be27fe
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-12 17:33:18 +0300

    Add product model and test creation to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello from client');
 });
 
-app.post('/api/products', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post('/api/products', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose.connect("mongodb+srv://admin:s1cBw30noRAWR09F@backenddb.8bf80.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
