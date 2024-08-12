5732c2d6d93217fe0d1cd58d6582c9cc1639b9f0
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-12 18:06:10 +0300

    Add single product fetching functionality

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 app.post('/api/products', async (req, res) => {
   try {
     const newProduct = await Product.create(req.body);
+    
     res.status(200).json(newProduct);
 
   } catch (error) {
@@ -22,11 +23,23 @@ app.post('/api/products', async (req, res) => {
 app.get('/api/products', async (req, res) => {
   try {
     const products = await Product.find({});
+
     res.status(200).json(products);
 
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
 
+app.get('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
