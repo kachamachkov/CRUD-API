da3c3574667709cd682fcdeeca221748db1bd3ec
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-12 18:22:21 +0300

    Add delete operation functionality

--- src/index.js
+++ src/index.js
@@ -62,6 +62,22 @@ app.put('/api/products/:id', async (req, res) => {
   }
 });
 
+app.delete('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 mongoose.connect("mongodb+srv://admin:s1cBw30noRAWR09F@backenddb.8bf80.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
   .then(() => {
