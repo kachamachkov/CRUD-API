b04a9a60b45268759211d5b678c1fec414b2a585
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-12 17:20:07 +0300

    Create product model

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
 app.get('/', (req, res) => {
-
   res.send('hello from client');
 });
-
 
 mongoose.connect("mongodb+srv://admin:s1cBw30noRAWR09F@backenddb.8bf80.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
   .then(() => {
     console.log('Connected to the database!');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
   })
   .catch(() => {
     console.log('Connection to the database failed!');
